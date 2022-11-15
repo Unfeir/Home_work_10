87eefa2f09faa24d78305f5fa867eebf98e251a7
Anton Holovin <unfeir@gmail.com>
2022-11-15 10:45:33 +0200

    ver2

--- homework_10.py
+++ homework_10.py
@@ -1,6 +1,6 @@
 from entitys import AddressBook, Record, Name, Phone
 
-my_dict = AddressBook()
+address_book = AddressBook()
 
 def input_error(func):
     """Всі помилки введення користувача повинні оброблятися за допомогою декоратора input_error. 
@@ -29,8 +29,8 @@ def input_error(func):
 
 def parser(text):
     if text:
-        a = text.replace("good bye", "good_bye").replace("show all", "show_all")
-        return a.split()[0], a.split()[1:] # формуємо кортеж із назви функції і аргументів для неї
+        normalise_text = text.replace("good bye", "good_bye").replace("show all", "show_all")
+        return normalise_text.split()[0], normalise_text.split()[1:] # формуємо кортеж із назви функції і аргументів для неї
     
               
 
@@ -52,17 +52,17 @@ def add_func(args):
     Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл."""
     name = args[0].capitalize()
     phones = args[1:]
-    if name in my_dict.data.keys():
+    if name in address_book.data.keys():
         for phone in phones:
-            if int(phone) not in my_dict[name].get_phones():
-                my_dict[name].add_phone(int(phone))
+            if int(phone) not in address_book[name].get_phones():
+                address_book[name].add_phone(int(phone))
         return f"{', '.join(phones)} was added to {name}" 
     else:
-        my_dict.add_record(Record(name))
+        address_book.add_record(Record(name))
         if phones:
             for phone in phones:
-                if int(phone) not in my_dict[name].get_phones():
-                    my_dict[name].add_phone(int(phone))
+                if int(phone) not in address_book[name].get_phones():
+                    address_book[name].add_phone(int(phone))
     
         return f"{name} was added with {', '.join(phones)}" 
 
@@ -74,7 +74,7 @@ def change_func(args):
     name = args[0].capitalize()
     phone = int(args[1])
     new_phone = int(args[2])
-    my_dict[name].change_phone(phone, new_phone)
+    address_book[name].change_phone(phone, new_phone)
     return f"{name}`s phone was changed from {phone} to {new_phone}"
     
        
@@ -83,7 +83,7 @@ def phone_func(args):
     """За цією командою бот виводить у консоль номер телефону для зазначеного контакту. 
     Замість ... користувач вводить ім'я контакту, чий номер треба показати."""
     name = args[0].capitalize()
-    all_phones = list(map(lambda x: str(x), my_dict[name].get_phones()))
+    all_phones = list(map(lambda x: str(x), address_book[name].get_phones()))
     return f"{name}`s phone number is {', '.join(all_phones)}"
 
 @input_error
@@ -92,8 +92,8 @@ def dell_func(args):
     Замість ... користувач вводить ім'я контакту, чий номер треба показати."""
     name = args[0].capitalize()
     phone = int(args[1])
-    if phone in my_dict[name].get_phones():
-        my_dict[name].del_phone(phone)
+    if phone in address_book[name].get_phones():
+        address_book[name].del_phone(phone)
         return f"{name}`s phone number '{phone}' was delleted"
     else:
         return f"{name} have no such number:'{phone}' "
@@ -103,9 +103,9 @@ def show_all_func(*args):
     """За цією командою бот виводить всі збереженні контакти з номерами телефонів у консоль."""
     contacts = ""
     
-    for k, v in my_dict.data.items():
+    for k, v in address_book.data.items():
         
-        contacts += f"{k} - {my_dict[k].get_phones()}\n"
+        contacts += f"{k} - {address_book[k].get_phones()}\n"
     
     return contacts
         
